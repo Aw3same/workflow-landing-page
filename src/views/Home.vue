1d19177630aab5c6b8ed1b968184f4cf657725d6
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2022-05-20 13:19:15 +0200

    Created home page and pricing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,146 @@
 <template>
-  <h1>WORKFLOW</h1>
   <div class="container">
-    <div class="flex flex-wrap lg:justify-between -mx-4">
-      <div class="w-full lg:w-1/2 xl:w-6/12 px-4">
-        <div class="max-w-[570px] mb-12 lg:mb-0">
-          <span class="block mb-4 text-base text-primary font-semibold">
-            Acerca el bienestar a tu empresa
-          </span>
-          <h2 class="text-dark mb-6 uppercase font-bold text-[32px] sm:text-[40px] lg:text-[36px] xl:text-[40px] ">
-            TU PLAN DE SALUD INTEGRAL
-          </h2>
-          <p class="text-base text-body-color leading-relaxed mb-9">
-            Para cualquier duda que tengas, escríbenos un mensaje y nuestro equipo se encargará de resolverla
-          </p>
+    <div class="relative pt-[120px] lg:pt-[150px] pb-[110px]">
+      <div class="container">
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full lg:w-5/12 px-4">
+            <div class="hero-content">
+              <h1 class="text-dark font-bold text-4xl sm:text-[42px] lg:text-[40px] xl:text-[42px] leading-snug mb-3">
+                Tu plan<br />
+                de salud<br />
+                integral.
+              </h1>
+              <p class="text-base mb-8 text-body-color max-w-[480px]">
+                Con Workflow, tu plan de salud integral estará a la vanguardia. Si quieres que tu empresa:
+              </p>
+              <ol>
+                <li class="flex text-base text-body-color mb-4">
+                  <span class="bg-gray-800 text-white rounded-full mr-2 text-base flex items-center justify-center h-6 max-w-[24px] w-full"> 1 </span>
+                  Mejore la salud y la satisfacción de los trabajadores.
+                </li>
+                <li class="flex text-base text-body-color mb-4">
+                  <span class="bg-gray-800 text-white rounded-full mr-2 text-base flex items-center justify-center h-6 max-w-[24px] w-full"> 2 </span>
+                  Reduzca el ausentismo laboral y el índice de rotación.
+                </li>
+                <li class="flex text-base text-body-color mb-4">
+                  <span class="bg-gray-800 text-white rounded-full mr-2 text-base flex items-center justify-center h-6 max-w-[24px] w-full"> 3 </span>
+                  Mejore la productividad de las personas en su puesto de trabajo y en consecuencia la productividad de la empresa.
+                </li>
+                <li class="flex text-base text-body-color mb-4">
+                  <span class="bg-gray-800 text-white rounded-full mr-2 text-base flex items-center justify-center h-6 max-w-[24px] w-full"> 4 </span>
+                  Reduzca los gastos en salud.
+                </li>
+                <li class="flex text-base text-body-color mb-4">
+                  <span class="bg-gray-800 text-white rounded-full mr-2 text-base flex items-center justify-center h-6 max-w-[24px] w-full"> 5 </span>
+                  Mejora el clima laboral, aumentando la participación y colaboración entre los trabajadores.
+                </li>
+              </ol>
+              <div class="clients pt-16">
+                <h6 class="font-normal text-xs flex items-center text-body-color mb-2">
+                  Alguno de nuestros clientes
+                  <span class="w-8 h-[1px] bg-body-color inline-block ml-2"></span>
+                </h6>
+                <div class="flex items-center">
+                  <div class="w-full py-3 mr-4">
+                    <img src="https://cdn.tailgrids.com/1.0/assets/images/brands/ayroui.svg" alt="ayroui" />
+                  </div>
+                  <div class="w-full py-3 mr-4">
+                    <img src="https://cdn.tailgrids.com/1.0/assets/images/brands/graygrids.svg" alt="graygrids" />
+                  </div>
+                  <div class="w-full py-3 mr-4">
+                    <img src="https://cdn.tailgrids.com/1.0/assets/images/brands/uideck.svg" alt="uideck" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="hidden lg:block lg:w-1/12 px-4"></div>
+          <div class="w-full lg:w-6/12 px-4">
+            <div class="lg:text-right lg:ml-auto">
+              <div class="relative inline-block z-10 pt-11 lg:pt-0">
+                <img src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png" alt="hero" class="max-w-full lg:ml-auto" />
+                <span class="absolute -left-8 -bottom-8 z-[-1]">
+                  <svg width="93" height="93" viewBox="0 0 93 93" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+                  </svg>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <!-- ====== Call To Action Section Start -->
+    <section class="py-10 lg:py-[120px]">
+      <div class="container">
+        <div class="bg-gray-800 relative rounded overflow-hidden py-12 px-8 md:p-[70px] z-10">
+          <div class="flex flex-wrap items-center -mx-4">
+            <div class="w-full lg:w-1/2 px-4">
+              <span class="text-white text-base font-semibold mb-2"> LLeva tu empresa al siguiente nivel</span>
+              <h2 class="text-white font-bold text-3xl sm:text-[38px] leading-tight mb-6 sm:mb-8 lg:mb-0">
+                Empieza a mejorar la calidad <br class="hidden xs:block" />
+                de vida de tus empleados
+              </h2>
+            </div>
+            <div class="w-full lg:w-1/2 px-4">
+              <div class="flex flex-wrap lg:justify-end">
+                <a href="javascript:void(0)" class="inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-[#13C296] transition hover:bg-opacity-90 text-white my-1">
+                  Empezar
+                </a>
+              </div>
+            </div>
+          </div>
+          <div>
+            <span class="absolute top-0 left-0 z-[-1]">
+              <svg width="189" height="162" viewBox="0 0 189 162" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <ellipse cx="16" cy="-16.5" rx="173" ry="178.5" transform="rotate(180 16 -16.5)" fill="url(#paint0_linear)" />
+                <defs>
+                  <linearGradient id="paint0_linear" x1="-157" y1="-107.754" x2="98.5011" y2="-106.425" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="white" stop-opacity="0.07" />
+                    <stop offset="1" stop-color="white" stop-opacity="0" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </span>
+            <span class="absolute bottom-0 right-0 z-[-1]">
+              <svg width="191" height="208" viewBox="0 0 191 208" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <ellipse cx="173" cy="178.5" rx="173" ry="178.5" fill="url(#paint0_linear)" />
+                <defs>
+                  <linearGradient id="paint0_linear" x1="-3.27832e-05" y1="87.2457" x2="255.501" y2="88.5747" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="white" stop-opacity="0.07" />
+                    <stop offset="1" stop-color="white" stop-opacity="0" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -24,7 +149,6 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'HomeComponent',
-  components: {
-  },
+  components: {},
 })
 </script>
--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -1,174 +1,90 @@
 <template>
-  <section class="pt-20 lg:pt-[120px] pb-12 lg:pb-[90px]">
+  <section class="pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] relative z-20 overflow-hidden">
     <div class="container">
       <div class="flex flex-wrap -mx-4">
         <div class="w-full px-4">
           <div class="text-center mx-auto mb-12 lg:mb-20 max-w-[510px]">
-            <span class="font-semibold text-lg text-primary mb-2 block">
-              Planes de precios
-            </span>
-            <h2 class="font-bold text-3xl  sm:text-4xl md:text-[40px] text-dark mb-4">
-              Nuestros planes
-            </h2>
-            <p class="text-base text-body-color">
-              Ofrecemos tres paquetes de precios en función de tus necesidades. Echa un vistazo y elige el que más te guste
-            </p>
+            <span class="font-semibold text-lg text-primary mb-2 block"> Planes de precios </span>
+            <h2 class="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">Nuestros planes</h2>
+            <p class="text-base text-body-color">Ofrecemos tres paquetes de precios en función de tus necesidades. Echa un vistazo y elige el que más te guste</p>
           </div>
         </div>
       </div>
-      <div class="flex flex-wrap -mx-4">
-        <section>
-          <div class="container max-w-full mx-auto py-2 px-6">
-            <div class="max-w-full md:max-w-6xl mx-auto my-3 md:px-8">
-              <div class="relative block flex flex-col md:flex-row items-center">
-                <!-- first price -->
-                <div class="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4">
-                  <div class="bg-white text-black rounded-lg border-t-4 border-indigo-600  shadow-lg overflow-hidden">
-                    <div class="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6">
-                      <h1 class="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide">
-                        Básico
-                      </h1>
-                      <h2 class="text-sm text-gray-500 text-center pb-6">
-                        <span class="text-2xl">500 €</span> / year
-                      </h2>
-
-                      Courses vary depending on the content. Contact us for more details.
-                    </div>
-
-                    <div class="flex flex-wrap mt-3 px-6">
-                      <ul>
-                        <li class="flex items-center">
-                          <div class=" rounded-full p-2 fill-current text-green-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                          </div>
-                          <span class="text-gray-700 text-lg ml-3">Up to 25 workers</span>
-                        </li>
-                        <li class="flex items-center">
-                          <div class=" rounded-full p-2 fill-current text-green-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                          </div>
-                          <span class="text-gray-700 text-lg ml-3">3 Free courses</span>
-                        </li>
-                        <li class="flex items-center">
-                          <div class=" rounded-full p-2 fill-current text-green-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                          </div>
-                          <span class="text-gray-700 text-lg ml-3">Lorem ipsun dolor sit amet</span>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="block flex items-center p-8  uppercase">
-                      <button class="w-full px-2 py-2 text-indigo-200 bg-indigo-600 rounded">Empezar</button>
-                    </div>
-                  </div>
-                </div>
-                <!-- Second price -->
-                <div class="w-full max-w-md sm:w-2/3 lg:w-1/3 sm:my-5 my-8 relative z-10 bg-white rounded-lg shadow-lg">
-                  <div class="text-sm leading-none rounded-t-lg bg-gray-800 text-white font-semibold uppercase py-4 text-center tracking-wide">
-                    Most Popular
-                  </div>
-                  <div class="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6">
-                    <h1 class="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide">
-                      Plus
-                    </h1>
-                    <h2 class="text-sm text-gray-500 text-center pb-6"><span class="text-3xl">700 €</span> / year</h2>
-
-                    Stripe offers everything needed to run an online business at scale. Get in touch for details.
-                  </div>
-                  <div class="flex pl-12 justify-start sm:justify-start mt-3">
-                    <ul>
-                      <li class="flex items-center">
-                        <div class=" rounded-full p-2 fill-current text-green-700">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                          </svg>
-                        </div>
-                        <span class="text-gray-700 text-lg ml-3">Up to 250 workers</span>
-                      </li>
-                      <li class="flex items-center">
-                        <div class=" rounded-full p-2 fill-current text-green-700">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                          </svg>
-                        </div>
-                        <span class="text-gray-700 text-lg ml-3">3 Free courses</span>
-                      </li>
-                      <li class="flex items-center">
-                        <div class=" rounded-full p-2 fill-current text-green-700">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                          </svg>
-                        </div>
-                        <span class="text-gray-700 text-lg ml-3">Lorem ipsun dolor sit amet</span>
-                      </li>
-                    </ul>
-                  </div>
-
-                  <div class="block flex items-center p-8  uppercase">
-                    <button class="w-full px-2 py-2 text-indigo-200 bg-indigo-600 rounded">Empezar</button>
-                  </div>
-                </div>
-                <!-- Price 3 -->
-                <div class="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-ml-4 border-t-4 border-indigo-600 ml-10">
-                  <div class="bg-white text-black rounded-lg shadow-lg overflow-hidden">
-                    <div class="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6">
-                      <h1 class="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide">
-                        Pro
-                      </h1>
-                      <h2 class="text-sm text-gray-500 text-center pb-6">
-                        <span class="text-2xl">900 €</span> / year
-                      </h2>
-
-                      Stripe offers everything needed to run an online business at scale. Get in touch for details.
-                    </div>
-                    <div class="flex flex-wrap mt-3 px-6">
-                      <ul>
-                        <li class="flex items-center">
-                          <div class=" rounded-full p-2 fill-current text-green-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                          </div>
-                          <span class="text-gray-700 text-lg ml-3">Up to 1000 workers</span>
-                        </li>
-                        <li class="flex items-center">
-                          <div class=" rounded-full p-2 fill-current text-green-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                          </div>
-                          <span class="text-gray-700 text-lg ml-3">3 Free courses</span>
-                        </li>
-                        <li class="flex items-center">
-                          <div class=" rounded-full p-2 fill-current text-green-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2 text-sm text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                          </div>
-                          <span class="text-gray-700 text-lg ml-3">Lorem ipsun dolor sit amet</span>
-                        </li>
-                      </ul>
-                    </div>
-
-                    <div class="block flex items-center p-8  uppercase">
-                      <button class="w-full px-2 py-2 text-indigo-200 bg-indigo-600 rounded">Empezar</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div class="flex flex-wrap justify-center -mx-4">
+        <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+          <div class="bg-white rounded-xl relative z-10 overflow-hidden border border-primary border-opacity-20 shadow-pricing py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12 mb-10">
+            <span class="text-primary font-semibold text-lg block mb-4"> Empresa 100 % </span>
+            <h2 class="font-bold text-dark mb-5 text-[42px]">
+              250€
+              <span class="text-base text-body-color font-medium"> / persona </span>
+            </h2>
+            <p class="text-base text-body-color pb-8 mb-8 border-b border-[#F2F2F2]">Todo incluido para el bienestar de tus empleados a un precio insuperable</p>
+            <div class="mb-7">
+              <p class="text-base text-body-color leading-loose mb-1">50-250 trabajadores</p>
+              <p class="text-base text-body-color leading-loose mb-1">Psicología</p>
+              <p class="text-base text-body-color leading-loose mb-1">Nutrición</p>
+              <p class="text-base text-body-color leading-loose mb-1">Fisioterapia</p>
+              <p class="text-base text-body-color leading-loose mb-1">Entrenador personal</p>
+              <p class="text-base text-body-color leading-loose mb-1">Talleres a precio especial a partir de 12 empleados</p>
+            </div>
+            <a
+              href="javascript:void(0)"
+              class="w-full block text-base font-semibold text-primary bg-transparent border border-[#D4DEFF] rounded-md text-center p-4 hover:text-white hover:bg-gray-800 hover:border-primary transition"
+            >
+              Elegir Empresa 100%
+            </a>
+            <div>
+              <span class="absolute right-0 top-7 z-[-1]">
+                <svg width="77" height="172" viewBox="0 0 77 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="86" cy="86" r="86" fill="url(#paint0_linear)" />
+                  <defs>
+                    <linearGradient id="paint0_linear" x1="86" y1="0" x2="86" y2="172" gradientUnits="userSpaceOnUse">
+                      <stop stop-color="#3056D3" stop-opacity="0.09" />
+                      <stop offset="1" stop-color="#C4C4C4" stop-opacity="0" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </span>
             </div>
           </div>
-        </section>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/3 px-4">
+          <div class="bg-white rounded-xl relative z-10 overflow-hidden border border-primary border-opacity-20 shadow-pricing py-10 px-8 sm:p-12 lg:py-10 lg:px-6 xl:p-12 mb-10">
+            <span class="text-primary font-semibold text-lg block mb-4"> Compartido </span>
+            <h2 class="font-bold text-dark mb-5 text-[42px]">
+              150€
+              <span class="text-base text-body-color font-medium"> / persona </span>
+            </h2>
+            <p class="text-base text-body-color pb-8 mb-8 border-b border-[#F2F2F2]">Asume el 60% del coste de cada servicio de salud para tus empleados.</p>
+            <div class="mb-7">
+              <p class="text-base text-body-color leading-loose mb-1">Primera sesión gratuita</p>
+              <p class="text-base text-body-color leading-loose mb-1">Psicología</p>
+              <p class="text-base text-body-color leading-loose mb-1">Nutrición</p>
+              <p class="text-base text-body-color leading-loose mb-1">Fisioterapia</p>
+              <p class="text-base text-body-color leading-loose mb-1">Entrenador personal</p>
+              <p class="text-base text-body-color leading-loose mb-1">Talleres a precio especial a partir de 10 empleados</p>
+            </div>
+            <a href="javascript:void(0)" class="w-full block text-base font-semibold text-white bg-gray-800 border border-primary rounded-md text-center p-4 hover:bg-opacity-90 transition">
+              Elegir Compartido
+            </a>
+            <div>
+              <span class="absolute right-0 top-7 z-[-1]">
+                <svg width="77" height="172" viewBox="0 0 77 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="86" cy="86" r="86" fill="url(#paint0_linear)" />
+                  <defs>
+                    <linearGradient id="paint0_linear" x1="86" y1="0" x2="86" y2="172" gradientUnits="userSpaceOnUse">
+                      <stop stop-color="#3056D3" stop-opacity="0.09" />
+                      <stop offset="1" stop-color="#C4C4C4" stop-opacity="0" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
-
 </template>
 
 <script lang="ts">
